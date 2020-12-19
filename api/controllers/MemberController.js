95435e5bc23b83ba28b607d06ab24df40207d3a6
paulsenjs <paulsenjs5@gmail.com>
2020-12-19 19:46:19 +0700

    remove member_id from upload files

--- api/controllers/MemberController.js
+++ api/controllers/MemberController.js
@@ -360,8 +360,6 @@ const MemberController = () => {
     };
 
     const upload_files = async (req, res) => {
-        const {member_id} = req.params;
-
         try {
             if (!req.files || Object.keys(req.files).length === 0) {
                 return res.status(400).send('No files were uploaded.');
